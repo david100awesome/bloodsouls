6b9e919bd487efb1ad3e98c4b5f6b5821cf1233d
david100awesome <32517214+david100awesome@users.noreply.github.com>
2017-10-07 14:37:02 -0500

    Add files via upload

--- bloodsouls.py
+++ bloodsouls.py
@@ -1,3 +1,4 @@
+import random
 minimum_age = 10
 x = ("vsvnsnvosobsnbo")
 print(x)
@@ -12,7 +13,7 @@ else:
 print("This is your first fight")
 minimum_number = 1550
 maximum_number = 1552
-x = ("sound volume")
+
 number = 0
 print(x)
 while number != 1551:
@@ -23,53 +24,71 @@ while number != 1551:
     else:
         break
         
-
 while True:
-    weapon = ("fist")
+    
+    weapon = ("stone_sword")
     health = 100
     fist = 1
     Enemy_health = 100
+    wolf_health = 20
     damage =  weapon
-    wooden_sword = 10
-    stone_sword = 20
-    print("you are in TN!")
-    
-    choice = input("You find a wooden sword. pick it up? (y or n)")
-    if choice == "y" or choice == "Y":
-        weapon = wooden_sword
-        print("You picked it up!")
-    print("Someone hits you")
-    health -= 10
-    print("You took 10 damage")
-    print("Health:", health)
-    print("you hit him")
-    Enemy_health -= 10
-    print("enemy retreated") 
-     
+    print("you are in TN")
+    choice = input("you find a enemy attack?you have to say yes sorry(y)")
+    if choice =="y" or choice =="Y":
+        while Enemy_health > 0 and health > 0:
+            p_damage = random.randint(1,  20)
+            e_damage = random.randint(1,  15)
+            
+            # player attacks
+            if p_damage < 5:
+                print("you missed")
+            else:
+                print("you hit him!" , p_damage)
+                Enemy_health -= p_damage 
+            print("enemy health" , Enemy_health)
+            if Enemy_health < 1:
+                continue
+            
+            # enemy attacks
+            if e_damage < 5:
+                print("the enemy missed")
+            else:
+                print("the enemy hit you",  e_damage) 
+                health -= e_damage 
+            print("your health" , health , )
+    else:
+        print("I told you you can't do that!")
+        continue
     choice = input("you find a health potion drink it? (y or n)")
     if choice == "y" or choice == "Y":
-        health += 10
-    print("the enemy came back!")
+        health = 100
+
     
-    choice = input("would you like to attack? (y or n)")
-    if choice == "y" or choice == "Y":
-        Enemy_health -= 10
-        print ("the enemy fell in a hole and died!hooray!")
-        Enemy_health -= 80
-    else:
-        print("the enemy hit you and you fell in a hole and died! NO!")
-        health -=  90
-        continue
+    
+    
+        
+    
+    
+        
+    
         
     choice = input("you find a house. go in?(y or n)")
     if choice == "y" or choice == "Y":
-        print("you are in the house")
-    else:
-        print("you are not in the house")
+            print("you are in the house")
+            print("ther is nothing in the house you leave the house.")
+    else:  
+            print("you are not in the house")
+            
+            print("you got shot by a enemy NO!")
+            continue
         
-    choice = input("you find a chest.open it?(y or n)")        
-    if choice == "y" or choice == "Y":
-            weapon = stone_sword
-            print("you now have a stone_sword")
+        
+    choice = input("you find a wolf.attack?(y or n)")
+    if choice == "y"or choice == "Y":
+            print("you hit the wolf in the head and it died!")
+            print("you win!")
             break
-       
+    else:
+            print("the wolf hit it's head on accident")
+            print("you win!")
+            break
